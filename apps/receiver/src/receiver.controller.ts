68e451afab06df34dcb8c49492d64b26ad9ff841
haridasan <bharidasan50@gmail.com>
2023-12-10 21:48:20 +0800

    update return value

--- apps/receiver/src/receiver.controller.ts
+++ apps/receiver/src/receiver.controller.ts
@@ -7,7 +7,7 @@ export class ReceiverController {
   constructor(private readonly receiverService: ReceiverService) {}
 
   @MessagePattern({ app: 'receiver', cmd: 'reply' })
-  async getMessage(@Payload() data: string): Promise<string> {
+  getMessage(@Payload() data: string): string {
     return this.receiverService.getMessage(data);
   }
 }
--- apps/sender/src/sender.service.ts
+++ apps/sender/src/sender.service.ts
@@ -1,19 +1,22 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { RECEIVER_SERVICE } from './services';
 import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class SenderService {
+  private readonly logger = new Logger(SenderService.name);
+
   constructor(
     @Inject(RECEIVER_SERVICE)
     private client: ClientProxy,
   ) {}
 
   async getHello(): Promise<string> {
+    this.logger.log(`Sending message to receiver`);
     const pattern = { app: 'receiver', cmd: 'reply' };
-    const { message } = await lastValueFrom(
-      this.client.send<{ message: string }>(pattern, 'hello from sender'),
+    const message = await lastValueFrom(
+      this.client.send<string, string>(pattern, 'hello from sender'),
     );
     return message;
   }
